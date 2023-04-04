a15310d984d31a0495857edc1a0957200d8a39f8
me <me>
2023-04-04 15:20:12 +0100

    Added languages

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -11,11 +11,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSelectModule } from '@angular/material/select';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-// import { CommonModule } from '@angular/common';
 import { UppercasePipe } from './pipes/uppercase.pipe';
 
 import { BoardService } from './services/board-taskList-service/board-taskList-service.service';
@@ -59,7 +58,6 @@ import { CreateStateComponent } from './components/create-state/create-state.com
     CreateStateComponent,
   ],
   imports: [
-    // CommonModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
@@ -70,6 +68,7 @@ import { CreateStateComponent } from './components/create-state/create-state.com
     MatInputModule,
     MatNativeDateModule,
     BrowserAnimationsModule,
+    MatSelectModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
